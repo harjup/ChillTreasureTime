2db6c4664a185702f474e565acb2152e8b9daf92
harjup <pikmin@gmail.com>
2016-01-18 13:42:34 -0500

    Player shadow sticks to level geometry

---
+++ src/ChillTeasureTime/Assets/src/scripts/Helper/StickToFloor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class StickToFloor : MonoBehaviour
+{
+    void Update()
+    {
+        Ray r = new Ray(GameObject.Find("Player(Clone)").transform.position, -Vector3.up);
+        RaycastHit hit;
+        if (Physics.Raycast(r, out hit))
+        {
+            if (hit.transform.GetComponent<LevelGeometry>() != null)
+            {
+                float targetY = hit.point.y;
+                transform.position = transform.position.SetY(targetY + .1f);
+            }
+        }
+    }
+}
