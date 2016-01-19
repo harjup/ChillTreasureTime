584075dcc6772bfc2fb68a0532d128820cb78ad7
harjup <pikmin@gmail.com>
2016-01-19 18:19:33 -0500

    Update beach snow song, add lighthouse and dance songs. crossfade songs between areas

--- src/ChillTeasureTime/Assets/src/scripts/Services/MusicService.cs
+++ src/ChillTeasureTime/Assets/src/scripts/Services/MusicService.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 using JetBrains.Annotations;
 
 public class MusicService : Singleton<MusicService>
 {
     public AudioClip BeachSnow;
+    public AudioClip Lighthouse;
     public AudioSource PrimaryAudioSource;
 
 
@@ -19,4 +21,43 @@ public class MusicService : Singleton<MusicService>
         PrimaryAudioSource.clip = BeachSnow;
         PrimaryAudioSource.Play();
     }
+
+    public void TransitionToLighthouse()
+    {
+        DOTween.Sequence()
+            .Append(PrimaryAudioSource.DOFade(0f, .5f))
+            .AppendCallback(() =>
+            {
+                PrimaryAudioSource.clip = Lighthouse;
+                PrimaryAudioSource.Play();
+            })
+            .Append(PrimaryAudioSource.DOFade(1f, 1f));
+    }
+
+    public void TransitionToBeachSnow()
+    {
+        DOTween.Sequence()
+            .Append(PrimaryAudioSource.DOFade(0f, .5f))
+            .AppendCallback(() =>
+            {
+                PrimaryAudioSource.clip = BeachSnow;
+                PrimaryAudioSource.Play();
+            })
+            .Append(PrimaryAudioSource.DOFade(1f, 1f));
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        if (Application.loadedLevelName == "Lighthouse")
+        {
+            TransitionToLighthouse();
+        }
+
+        if (Application.loadedLevelName == "Main")
+        {
+            TransitionToBeachSnow();
+        }
+    }
+
+    
 }
