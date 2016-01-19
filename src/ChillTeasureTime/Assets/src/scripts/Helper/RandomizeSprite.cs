113f62d3dde0a8f0d5543ec1f42f2682f63e8292
harjup <pikmin@gmail.com>
2016-01-18 19:17:41 -0500

    Randomize sprites for treasure put into nest

---
+++ src/ChillTeasureTime/Assets/src/scripts/Helper/RandomizeSprite.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RandomizeSprite : MonoBehaviour
+{
+    public List<Sprite> Sprites;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = Sprites.AsRandom().ToList().First();
+    }
+}
