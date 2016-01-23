2697ad5280df59605568d4f6b0d090b80f9e6dfd
harjup <pikmin@gmail.com>
2016-01-22 19:08:10 -0500

    Update visuals for dance section

---
+++ src/ChillTeasureTime/Assets/src/scripts/Helper/AlternateVisibilityOnBeat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class AlternateVisibilityOnBeat : MonoBehaviour, IBeat, IBar
+{
+
+    public GameObject UpObject;
+    public GameObject DownObject;
+
+    public void OnBeat(int bar, int beat)
+    {
+        UpObject.SetActive(!UpObject.activeSelf);
+        DownObject.SetActive(!DownObject.activeSelf);
+    }
+
+    public void OnBar(int bar)
+    {
+        transform.DOPunchPosition(Vector3.down * .1f, .1f);
+    }
+}
