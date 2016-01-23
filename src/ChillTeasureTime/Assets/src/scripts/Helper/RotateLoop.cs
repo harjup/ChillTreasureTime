9e63da4c273bfd1d5fc71a8049a3346d327e2267
harjup <pikmin@gmail.com>
2016-01-22 23:16:41 -0500

    It's overrrr. ALso added lighthouse light that spins.

---
+++ src/ChillTeasureTime/Assets/src/scripts/Helper/RotateLoop.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class RotateLoop : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        transform
+            .DOLocalRotate(transform.eulerAngles + new Vector3(0f, 360f, 0f), 4f, RotateMode.FastBeyond360).SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Incremental);
+    }
+}
