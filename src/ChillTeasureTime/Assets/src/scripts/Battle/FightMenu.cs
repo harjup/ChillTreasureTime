d32395d88b4e58027a97223361111bb03a937f5d
harjup <pikmin@gmail.com>
2016-02-20 15:40:47 -0500

    Add placeholder battle icons, put them on a menu wheel

---
+++ src/ChillTeasureTime/Assets/src/scripts/Battle/FightMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEditor;
+
+public class FightMenu : MonoBehaviour
+{
+
+    private GameObject FightMenuItemPrefab;
+
+    public List<FightMenuItem> MenuItems = new List<FightMenuItem>();
+
+    public List<MenuItemPosition> MenuItemPositions = new List<MenuItemPosition>(); 
+
+    void Start()
+    {
+        var menuItemKinds = FightMenuItem.GetAllKinds();
+
+        var initialPosition = transform.position;
+        var initialRadians = 0;
+        var radianIncrement = Mathf.PI / 6;
+        var radius = 2.5f;
+
+
+        FightMenuItemPrefab = Resources.Load<GameObject>("Prefabs/Battle/MenuItem-Default");
+
+        for (int i = 0; i < menuItemKinds.Count; i++)
+        {
+            var res = Instantiate(FightMenuItemPrefab);
+            var menuItem = res.GetComponent<FightMenuItem>();
+            menuItem.SetKind(menuItemKinds[i]);
+            menuItem.transform.parent = transform;
+            MenuItems.Add(menuItem);
+
+            var x = radius*Mathf.Cos(initialRadians + (radianIncrement*i));
+            var y = radius*Mathf.Sin(initialRadians + (radianIncrement*i));
+
+            menuItem.transform.position = transform.position + new Vector3(x, y, 0f);
+        }
+    }
+
+
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.DORotate(transform.rotation.eulerAngles + Vector3.zero.SetZ(-30f), .25f);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.DORotate(transform.rotation.eulerAngles + Vector3.zero.SetZ(30f), .25f);
+        }
+    }
+}
---
+++ src/ChillTeasureTime/Assets/src/scripts/Battle/FightMenuItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FightMenuItem : MonoBehaviour
+{
+    public enum Kind
+    {
+        None,
+        Flap,
+        Peck,
+        Item,
+        Run
+    }
+
+    public static List<Kind> GetAllKinds()
+    {
+        return new List<Kind>()
+        {
+            Kind.Flap,
+            Kind.Peck,
+            Kind.Item,
+            Kind.Run
+        };
+    }
+    
+    private Dictionary<Kind, string> _kingToSpriteMap = new Dictionary<Kind, string>
+    {
+      {Kind.Flap, "Battle/MenuIcon/Battle-Flap" },
+      {Kind.Peck, "Battle/MenuIcon/Battle-Peck" },
+      {Kind.Item, "Battle/MenuIcon/Battle-Peach" },
+      {Kind.Run, "Battle/MenuIcon/Battle-Run" }
+    };
+
+    public Kind WhichKind;
+
+    private SpriteRenderer _spriteRenderer;
+
+    public void SetKind(Kind kind)
+    {
+        WhichKind = kind;
+        var spritePath = _kingToSpriteMap[kind];
+
+        var sprite = Resources.Load<Sprite>(spritePath);
+        if (sprite == null)
+        {
+            Debug.LogError("Sprite not found at path " + spritePath);
+        }
+
+        if (_spriteRenderer == null)
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+
+        gameObject.name = gameObject.name.Split('-')[0] + "-" + kind;
+
+        _spriteRenderer.sprite = sprite;
+    }
+
+    public void Start()
+    {
+        if (WhichKind != Kind.None)
+        {
+            SetKind(WhichKind);
+        }
+    }
+
+    public void Update()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
