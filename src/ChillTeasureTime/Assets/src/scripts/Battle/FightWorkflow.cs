df6171f72b9bf65a3a0441693fec480a362d18a4
harjup <pikmin@gmail.com>
2016-03-14 21:28:39 -0400

    Fiddle with sequencing a jump attack

--- src/ChillTeasureTime/Assets/src/scripts/Battle/FightWorkflow.cs
+++ src/ChillTeasureTime/Assets/src/scripts/Battle/FightWorkflow.cs
@@ -12,23 +12,85 @@ public class FightWorkflow : MonoBehaviour
 
     private FightEnemyManager _fightEnemyManager;
 
+    private bool _listenForJumpInput = false;
+    private bool _inputHit = false;
+
+    public float WalkUpTime = .5f;
+    public float PauseTime = .125f;
+    public float FirstJumpTime = .5f;
+    public float SecondJumpTime = .5f;
+    public float JumpOffTime = .25f;
+
+
     public void InitAttackSequence(GameObject target, string currentMove)
     {
         var initialPosition = Player.transform.position;
 
+        // Let's do a jump attack
+        // 1. Walk 
+        var enemyPosition = target.transform.position;
+
+        var playerHeight = 1.2f;
+        var playerWidth = 1f;
+        
+
+        //TODO: Specify where the "head" is on each enemy. Maybe assume body if no head.
+        var enemyHead = target.transform.position;
+        var stopPosition = enemyHead.SetY(Player.transform.position.y).SetX(enemyHead.x - (playerWidth * 3));
+
         DOTween
             .Sequence()
-            .Append(Player.transform.DOMove(target.transform.position, 1f))
-            .AppendCallback(() =>
+            .Append(Player.transform.DOMove(stopPosition, WalkUpTime).SetEase(Ease.Linear))
+            .AppendInterval(PauseTime)
+            .Append(Player.transform.DOJump(enemyHead, playerHeight * 2f, 1, FirstJumpTime).SetEase(Ease.Linear))
+            .InsertCallback(WalkUpTime + FirstJumpTime, () =>
             {
-                target.GetComponent<CharacterStatus>().AddDamage(1);
+                _listenForJumpInput = true;
             })
-            .Append(Player.transform.DOMove(initialPosition, 1f))
-            .AppendCallback(() =>
+            .InsertCallback(WalkUpTime + PauseTime + FirstJumpTime, () =>
             {
-                StartCoroutine(_fightEnemyManager.DoEnemyTurns());
+                _listenForJumpInput = false;
+                target.GetComponent<CharacterStatus>().AddDamage(1);
+                if (_inputHit)
+                {
+                    _inputHit = false;
+
+                    DOTween
+                        .Sequence()
+                        .Append(Player.transform.DOJump(enemyHead, playerHeight * 2f, 1, SecondJumpTime).SetEase(Ease.Linear))
+                        .AppendCallback(() =>
+                        {
+                            target.GetComponent<CharacterStatus>().AddDamage(1);
+                        })
+                        .Append(Player.transform.DOJump(stopPosition, playerHeight, 1, JumpOffTime).SetEase(Ease.Linear))
+                        .Append(Player.transform.DOMove(initialPosition, 1f).SetEase(Ease.Linear))
+                        .AppendCallback(() =>
+                        {
+                            // Lets ignore the enemy turns for now.
+                            // StartCoroutine(_fightEnemyManager.DoEnemyTurns());
+                            _fightMenu.Enable();
+                        });
+                }
+                else
+                {
+                    DOTween
+                      .Sequence()
+                      .Append(Player.transform.DOJump(stopPosition, playerHeight, 1, JumpOffTime).SetEase(Ease.Linear))
+                      .Append(Player.transform.DOMove(initialPosition, 1f).SetEase(Ease.Linear))
+                      .AppendCallback(() =>
+                      {
+                          // Lets ignore the enemy turns for now.
+                          // StartCoroutine(_fightEnemyManager.DoEnemyTurns());
+                          _fightMenu.Enable();
+                      });
+                }
             });
-    }
+
+        
+
+
+
+    }     
 
     // Play intro
     // Set up player, enemies
@@ -58,7 +120,10 @@ public class FightWorkflow : MonoBehaviour
 
     void Update()
     {
-
+        if (_listenForJumpInput && Input.GetKey(KeyCode.X))
+        {
+            _inputHit = true;
+        }
     }
 }
 
